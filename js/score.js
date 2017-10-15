b28fcc5f714ef0bc3b9d232dc3bdce474cfba754
BrunoReiss <brunoreis@mail.com>
2017-10-15 03:50:10 -0200

    Added some more js
    Added some more js and altered some variables' names.

--- js/score.js
+++ js/score.js
@@ -1,12 +1,19 @@
-var player1Button = document.getElementById("p1");
-var player2Button = document.getElementById("p2");
+var p1Button = document.getElementById("p1");
+var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 
-player1Button.addEventListener("click", function(){
-	alert("CLICKED!!!")
+var p1Score = 1;
+
+var h1 = document.querySelector("h1");
+
+var p1Display = document.getElementById("p1Display");
+var p2Display = document.getElementById("p2Display");
+
+p1Button.addEventListener("click", function(){
+	h1.textContent = p1Score++
 });
 
-player2Button.addEventListener("click", function(){
+p2Button.addEventListener("click", function(){
 	alert("CLICKED!!!")
 });
 
