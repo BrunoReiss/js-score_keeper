66741b1c2d5c1066d68ff5054fd3ad110e6b2b8f
BrunoReiss <brunoreis@mail.com>
2017-10-15 03:28:50 -0200

    Added buttons
    Added 'player1', 'player2' and 'reset' buttons and tested their connection to the js file.

--- js/score.js
+++ js/score.js
@@ -1,1 +1,16 @@
-alert("CONNECTED!!!")
+var player1Button = document.getElementById("p1");
+var player2Button = document.getElementById("p2");
+var resetButton = document.getElementById("reset");
+
+player1Button.addEventListener("click", function(){
+	alert("CLICKED!!!")
+});
+
+player2Button.addEventListener("click", function(){
+	alert("CLICKED!!!")
+});
+
+resetButton.addEventListener("click", function(){
+	alert("CLICKED!!!")
+})
+
