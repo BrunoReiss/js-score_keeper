ccc2452c458436ef0ce61af185a77160db732a81
BrunoReiss <brunoreis@mail.com>
2017-10-15 03:57:48 -0200

    Buttons working independently
    'Player1' and 'player2' buttons are now working independently and incrementing the score of each by 1 when clicked.

--- js/score.js
+++ js/score.js
@@ -2,19 +2,18 @@ var p1Button = document.getElementById("p1");
 var p2Button = document.getElementById("p2");
 var resetButton = document.getElementById("reset");
 
-var p1Score = 1;
-
-var h1 = document.querySelector("h1");
-
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
 
+var p1Score = 1;
+var p2Score = 1;
+
 p1Button.addEventListener("click", function(){
-	h1.textContent = p1Score++
+	p1Display.textContent = p1Score++
 });
 
 p2Button.addEventListener("click", function(){
-	alert("CLICKED!!!")
+	p2Display.textContent = p2Score++
 });
 
 resetButton.addEventListener("click", function(){
