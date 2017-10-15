cb251e32d70f437398b4bc88b663c84e13b16859
BrunoReiss <brunoreis@mail.com>
2017-10-15 04:25:58 -0200

    Added css file and button logic
    Added the score.css file and logic for the 'player1' and 'player2' buttons.

--- js/score.js
+++ js/score.js
@@ -5,15 +5,30 @@ var resetButton = document.getElementById("reset");
 var p1Display = document.getElementById("p1Display");
 var p2Display = document.getElementById("p2Display");
 
-var p1Score = 1;
-var p2Score = 1;
+var p1Score = 0;
+var p2Score = 0;
+
+var gameOver = false;
+var winningScore = 5;
 
 p1Button.addEventListener("click", function(){
-	p1Display.textContent = p1Score++
+	if(!gameOver){
+		p1Score++;
+		if(p1Score === winningScore){
+			gameOver = true;
+		}
+		p1Display.textContent = p1Score;
+	}
 });
 
 p2Button.addEventListener("click", function(){
-	p2Display.textContent = p2Score++
+	if(!gameOver){
+		p2Score++;
+		if(p2Score === winningScore){
+			gameOver = true;
+		}
+		p2Display.textContent = p2Score;
+	}
 });
 
 resetButton.addEventListener("click", function(){
